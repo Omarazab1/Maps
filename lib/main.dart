689f6a9b488d7ad5ae3bf8bf594387dc047cd373
Omarazab1 <oazab6806@gmail.com>
2024-08-11 18:55:05 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyMap());
+}
+
+
+ class MyMap extends StatelessWidget {
+   const MyMap({super.key});
+
+   @override
+   Widget build(BuildContext context) {
+     return const Placeholder();
+   }
+ }
+
